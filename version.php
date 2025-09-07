88f0a46f7a998de25ef7e1aadbdf17ef64015cdd
Andreas Grabs <moodle@grabs-edv.de>
2025-09-07 14:01:18 +0200

    mod_etherpadlite: update version and changelog

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024071001;  // The current module version (Date: YYYYMMDDXX).
-$plugin->release   = 'v4.4.1 (2025041500)';
+$plugin->version   = 2024071100;  // The current module version (Date: YYYYMMDDXX).
+$plugin->release   = 'v4.4.2 (2025090700)';
 $plugin->requires  = 2024041600;
 $plugin->component = 'mod_etherpadlite';
 $plugin->maturity  = MATURITY_STABLE;
